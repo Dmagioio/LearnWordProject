ebf556ef7d73f7a38453ee2ca53fbcad073088c6
marki <mariyan05@gmail.com>
2025-10-06 21:46:54 +0300

    processing the correct answer

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.example.myapplication.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,11 +21,70 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityLearnWordBinding.inflate(layoutInflater)
         setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_learn_word)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.learn)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.layoutAnswer3.setOnClickListener {
+            markAnswerCorrect()
+        }
+    }
+    private fun markAnswerCorrect() {
+        binding.layoutAnswer3.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_correct
+        )
+
+        binding.tvVariantNumber3.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_variants_correct
+        )
+
+        binding.tvVariantNumber3.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.white
+            )
+        )
+
+        binding.tvVariantValue3.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+        binding.btnSkip.isVisible = false
+
+        binding.layoutResult.setBackgroundColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+        binding.ivResultIcon.setImageDrawable(
+            ContextCompat.getDrawable(
+                this@MainActivity,
+                R.drawable.ic_correct
+            )
+        )
+
+        binding.tvResultMessage.text = resources.getString(R.string.title_correct)
+
+        binding.btnContinue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+        binding.layoutResult.isVisible = true
+
+
+
     }
 }
