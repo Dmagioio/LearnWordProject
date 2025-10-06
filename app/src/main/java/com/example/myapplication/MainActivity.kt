33f980cdf8211ad6605902f79291ec1057dfd4da
marki <mariyan05@gmail.com>
2025-10-06 20:23:29 +0300

    add ViewBinding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,10 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityLearnWordBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding Error")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityLearnWordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
         setContentView(R.layout.activity_learn_word)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.learn)) { v, insets ->
