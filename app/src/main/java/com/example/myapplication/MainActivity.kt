51e09d66de1d0f0070021e12afea4995d1f5441a
marki <markian3838@gmail.com>
2025-10-08 12:21:59 +0300

    add neutral variants

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -35,6 +35,49 @@ class MainActivity : AppCompatActivity() {
         binding.layoutAnswer1.setOnClickListener {
             markAnswerWrong()
         }
+
+        binding.btnContinue.setOnClickListener {
+            markAnswerNeutral()
+        }
+    }
+
+    private fun MainActivity.markAnswerNeutral() {
+
+        with(binding){
+
+            for (layout in listOf(layoutAnswer1, layoutAnswer3)) {
+                layout.background = ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.shape_rounded_containers
+                )
+            }
+
+            for (textView in listOf(tvVariantValue1, tvVariantValue3)) {
+                textView.setTextColor(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.textVariantsColor
+                    )
+                )
+            }
+            for (textView in listOf(tvVariantNumber1, tvVariantNumber3)) {
+                textView.apply {
+                    background = ContextCompat.getDrawable(
+                        this@MainActivity,
+                        R.drawable.shape_rounded_variants,
+                    )
+                    setTextColor(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.textVariantsColor
+                        )
+                    )
+                }
+            }
+
+            layoutResult.isVisible = false
+            btnSkip.isVisible = true
+        }
     }
 
     private fun markAnswerWrong() {
@@ -145,5 +188,3 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
-
-
