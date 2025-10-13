d78e7382ba4cfc4910577b57cda987830afe4477
marki <markian3838@gmail.com>
2025-10-13 17:37:50 +0300

    learning Lifecycle and Logcat

--- app/src/main/java/com/example/myapplication/SecondDemoActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondDemoActivity.kt
@@ -1,9 +1,12 @@
 package com.example.myapplication
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,12 +14,33 @@ import com.example.myapplication.databinding.ActivityFirstDemoBinding
 import com.example.myapplication.databinding.ActivitySecondDemoBinding
 
 class SecondDemoActivity : AppCompatActivity() {
-        private lateinit var binding: ActivitySecondDemoBinding
+    private lateinit var binding: ActivitySecondDemoBinding
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    private val locationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                Log.d("SecondDemoActivity", "Дозвіл на локацію включено")
+            } else {
+                Log.d("SecondDemoActivity", "Дозвіл на локацію вимкнено")
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             binding = ActivitySecondDemoBinding.inflate(layoutInflater)
             setContentView(binding.root)
+
+            Log.i("!!!", "${this.componentName.shortClassName} Виконується: onCreate() ")
+
+            val btnLocation = binding.btnRequestPermission
+            btnLocation.setOnClickListener{
+                locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+            }
+
+
+
+
+
 
             with(binding) {
 
@@ -54,4 +78,29 @@ class SecondDemoActivity : AppCompatActivity() {
                 tvWord.text = word?.original
             }
         }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("!!!", "${this.componentName.shortClassName} Виконується: onStart() ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("!!!", "${this.componentName.shortClassName} Виконується: onResume() ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("!!!", "${this.componentName.shortClassName} Виконується: onPause() ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("!!!", "${this.componentName.shortClassName} Виконується: onStop() ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("!!!", "${this.componentName.shortClassName} Виконується: onDestroy() ")
+    }
 }
